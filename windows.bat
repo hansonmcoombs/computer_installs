b56c7354faa98052f042ef31b7577b25d6b47072
Matt H <hansonmcoombs@gmail.com>
2022-01-06 16:11:05 +1300

    Create windows.bat

---
+++ windows.bat
@@ -0,0 +1,14 @@
+winget install --force --id Google.Chrome
+winget install --id Google.Drive
+winget install --id OSGeo.QGIS_LTR
+winget install --id Anaconda.Miniconda3
+winget install --id Notepad++.Notepad++
+winget install --id git.git
+winget install --id jetbrains.pycharm.community
+winget install --id manictime.manictime
+winget install --id bitwarden.bitwarden
+winget install --id 7zip.7zip
+winget install --id inkscape.inkscape
+
+:: todo
+:: ms office (if needed)
