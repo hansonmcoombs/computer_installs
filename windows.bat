9ea8808ceaf527bf4bb62444f37c008d776371bc
Matt D <hansonmcoombs@gmail.com>
2022-09-26 13:17:25 +1300

    adjust for feflow challenges

--- windows.bat
+++ windows.bat
@@ -1,5 +1,4 @@
 :: run command prompt as admin
-
 winget install --force --id Google.Chrome
 winget install --id Google.Drive
 winget install --id OSGeo.QGIS_LTR
@@ -7,12 +6,15 @@ winget install --id Anaconda.Miniconda3
 winget install --id Notepad++.Notepad++
 winget install --id git.git
 winget install --id jetbrains.pycharm.community
-winget install --id manictime.manictime
 winget install --id bitwarden.bitwarden
 winget install --id 7zip.7zip
 winget install --id inkscape.inkscape
-winget install --id Microsoft.Office
 winget install -e --id tailscale.tailscale
+
+:: optional installs
+:: winget install --id Microsoft.Office  :: this will break feflow 7.2.5
+:: winget install --id manictime.manictime :: unsure if this will break feflow 7.2.5
+
 :: net user USER_NAME PASSWORD /add
 :: net localgroup administrators USER_ACCOUNT /add
 
